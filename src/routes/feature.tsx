ab8b1f945f61fdd62f6f82be0ef56589f957143d
twiztss <paritch.kae@gmail.com>
2025-02-23 23:04:31 +0700

    Small fix on feature page.

--- src/routes/feature.tsx
+++ src/routes/feature.tsx
@@ -1,5 +1,5 @@
 import Header from "../components/Header"
-import { Compass, Computer, FastForward, InspectionPanel, Origami, Terminal } from "lucide-react"
+import { ArrowRight, Compass, Computer, FastForward, InspectionPanel, Origami, Terminal } from "lucide-react"
 import FooterFull from "../components/FooterFull"
 
 const data : object[] = [
@@ -48,36 +48,63 @@ type featureProps = {
   url : string,
 }
 
-const FeatureCard = ({ data } : any) => {
-  
-  const renderCard = (feature : featureProps) => {
-    return (
-      <div className="bg-white flex flex-col px-4 py-8 rounded-lg drop-shadow-lg w-1/4 gap-2 items-center" key = {feature.id}>
-        <div className="bg-gray-50 w-full aspect-video rounded-md"></div>
-        <FastForward width={64} height={64} className="-mt-8 bg-gray-100 p-4 rounded-full"/>
-        <img src={feature.url? feature.url : ""} alt="" />
-        <div className="flex flex-col text-center w-1/2 gap-2">
-          <h1 className = "text-2xl font-bold">{feature.name}</h1>
-          <p className="text-gray-400 text-md font-normal">{feature.description}</p>
-        </div>
+const FeatureCard = (feature : featureProps) => {
+  return (
+    <div className="bg-white flex flex-col px-4 py-8 rounded-lg drop-shadow-lg w-1/4 gap-2 items-center" key = {feature.id}>
+      <div className="bg-gray-50 w-full aspect-video rounded-md"></div>
+      <FastForward width={64} height={64} className="-mt-8 bg-gray-100 p-4 rounded-full"/>
+      <img src={feature.url? feature.url : ""} alt="" />
+      <div className="flex flex-col text-center w-1/2 gap-2">
+        <h1 className = "text-2xl font-bold">{feature.name}</h1>
+        <p className="text-gray-400 text-md font-normal">{feature.description}</p>
       </div>
-    )
-  } 
+    </div>
+  )
+} 
+
+const FeatureSection = ({ data } : any) => {
 
   return (
     <section className="flex flex-col m-10 gap-10 self-center items-center w-full">
       <h2 className="text-3xl font-bold">Why this product?</h2>
       <div className="flex px-8 py-4 gap-10 w-full justify-center flex-wrap"> 
-        {data.map(renderCard)}
+        {data.map(FeatureCard)}
       </div>
     </section>
+  )
+}
+
+const Feature2Card = (feature : featureProps) => {
+  return (
+    <article className="flex flex-col w-5/12 bg-gray-100 rounded-2xl aspect-video p-8 justify-end">
+      <div className="flex flex-col justify-center gap-4">
+        <h1 className="font-bold text-4xl">{feature.name + ".1"}</h1>
+        <p className="font-normal text-lg">{feature.description.length > 40 ? feature.description.slice(0,40) : feature.description}</p>
+        <button className="bg-gray-200 rounded-full px-4 py-2 w-1/4 flex gap-2 justify-center items-center">
+          <p className="font-bold text-lg">Explore</p>
+          <ArrowRight width={16} height={16} />
+        </button>
+      </div>
+    </article>
+  )
+}
+
+const FeatureSection2 = ({ data } : any) => {
+
+  return (
+    <>
+      <h2 className="text-3xl font-bold self-center my-5">What's new?</h2>
+      <section className="flex px-8 py-4 gap-6 w-full mx-10 justify-center self-center flex-wrap"> 
+        {data.slice(0,4).map(Feature2Card)}
+      </section>
+    </>
   )
 }
 
 const Sponsor = () => {
   
   return (
-    <section className="flex flex-col px-10 py-4 items-center gap-12">
+    <section className="flex flex-col px-10 py-4 items-center gap-12 my-10">
           <h1 className="text-3xl font-bold">Sponsored by</h1>
           <div className="flex justify-between w-1/2">
             <Origami width={32} height={32}/> 
@@ -104,7 +131,8 @@ export default function Feature() {
           </div>
         </div>  
       </section>
-      <FeatureCard data = {data}/>
+      <FeatureSection data = {data}/>
+      <FeatureSection2 data={data} />
       <Sponsor />
 
       <FooterFull />
